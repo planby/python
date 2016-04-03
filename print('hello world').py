d64a06220aa17f987bc6397c40110603a804a20c
planby <planby@126.com>
2016-04-03 10:24:45 +0800

    hello world test

--- print('hello world').py
+++ print('hello world').py
@@ -1,1 +1,6 @@
-print('hello world')
+print('this is the NSA system，hello world')
+name = input('enter your name here:')
+if name = 'ccnn'
+    print('Hello', name)
+    else
+    print('this user is not existed')
